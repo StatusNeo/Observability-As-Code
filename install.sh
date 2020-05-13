a2378ee02cb5e8016730a9a1163d5f28a7f350db
Shreya Singh <32371715+shreyasingh18@users.noreply.github.com>
2020-05-13 19:56:27 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+# Install Python and configure python
+apt install -y python3
+apt install -y python3-pip
+pip3 install tweepy
+pip3 install pandas
+
+# Install Java
+apt install -y openjdk-11-jre-headless
+
+# Install Maven
+apt install -y maven
+
