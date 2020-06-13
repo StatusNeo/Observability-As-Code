c18714fee8ab51647d381d5d7884d06452da7430
NishkarshRaj <500060720@stu.upes.ac.in>
2020-06-13 15:13:56 +0530

    Push changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ resource "aws_key_pair" "sshkey" {
 }
 
 resource "aws_instance" "ec2" {
-  ami           = "ami-063246c1e8988282c"
+  ami           = "ami-0eab02c550173a8a4"
   instance_type = "${var.vm_size}"
   subnet_id = "${aws_subnet.subnet[0].id}"
   associate_public_ip_address = true
